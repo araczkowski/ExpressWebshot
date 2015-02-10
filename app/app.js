0185ca13493b327012c87e60265da956ab4d3919
Andrzej Raczkowski <araczkowski@gmail.com>
2015-02-10 09:21:54 +0100

    GraphicsMagick for node.js

--- app/app.js
+++ app/app.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var app = express();
 var webshot = require('webshot');
+
+//var thief = require('color-thief');
+var gm = require('gm');
+
 var fs = require('fs');
 var fileName, timeStart, timeStop, ServHost, ServPort;
-
-
+var imgOptions = {
+    screenSize: {
+        width: 1000,
+        height: 480
+    },
+    shotSize: {
+        width: 1000,
+        height: 'all'
+    },
+    defaultWhiteBackground: true
+}
 
 // a middleware with no mount path; gets executed for every request to the app
 /*
@@ -15,18 +28,28 @@ app.use(function (req, res, next) {
 });*/
 
 
+function logStart() {
+    timeStart = Date.now();
+    fileName = timeStart + '.jpeg';
+    console.log('File Name:', fileName);
+    console.log('Time Start:', timeStart);
+};
+
+function logEnd() {
+    timeStop = Date.now();
+    console.log('Time Stop:', timeStop);
+    var differenceMs = timeStop - timeStart;
+    console.log('Execution time [s]:', differenceMs / 1000);
+    console.log('OK');
+};
 
 
 app.get('/blocks', function (req, res) {
-
-    // log start
-    timeStart = Date.now();
-    fileName = timeStart + '.png';
-    console.log('File Name:', fileName);
-    console.log('Time Start:', timeStart);
+    //log start
+    logStart();
 
     // render
-    webshot('http://' + ServHost + ':' + ServPort + '/index.html', function (err, renderStream) {
+    webshot('http://' + ServHost + ':' + ServPort + '/index.html', imgOptions, function (err, renderStream) {
         var file = fs.createWriteStream('temp/' + fileName, {
             encoding: 'binary'
         });
@@ -36,18 +59,53 @@ app.get('/blocks', function (req, res) {
         });
 
         renderStream.on('end', function () {
+
             res.writeHead(200, {
                 'Content-Type': 'image/png'
             });
+
+
             var stream = fs.createReadStream('temp/' + fileName);
             stream.pipe(res);
 
             // log end
-            timeStop = Date.now();
-            console.log('Time Stop:', timeStop);
-            var differenceMs = timeStop - timeStart;
-            console.log('Execution time [s]:', differenceMs / 1000);
-            console.log('OK');
+            logEnd();
+        });
+    });
+});
+
+
+app.get('/blocksbw', function (req, res) {
+
+    //log start
+    logStart();
+
+    // render
+    webshot('http://' + ServHost + ':' + ServPort + '/index.html', imgOptions, function (err, renderStream) {
+        var file = fs.createWriteStream('temp/' + fileName, {
+            encoding: 'binary'
+        });
+        renderStream.on('data', function (data) {
+            file.write(data.toString('binary'), 'binary');
+
+        });
+
+        renderStream.on('end', function () {
+
+            res.writeHead(200, {
+                'Content-Type': 'image/png'
+            });
+
+
+            gm('temp/' + fileName)
+                .type('Grayscale')
+                //.monochrome()
+                //.colors(2)
+                .stream(function streamOut(err, stdout, stderr) {
+                    stdout.pipe(res); //pipe to response
+                });
+            // log end
+            logEnd();
 
 
         });
