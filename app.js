5e09216eabb614a209100906c1f6db0501841aac
Andrzej Raczkowski <araczkowski@gmail.com>
2016-03-09 21:04:55 +0100

    charts version

--- app.js
+++ app.js
@@ -8,103 +8,117 @@ var webshot = require('webshot');
 var timeStart, timeStop;
 
 var internalOptions = {
-    screenSize: {
-        width: 'all',
-        height: 'all'
-    },
-    shotSize: {
-        width: 'all',
-        height: 'all'
-    },
-    defaultWhiteBackground: true,
-    shotUrl: ''
+  screenSize: {
+    width: 'all',
+    height: 'all'
+  },
+  shotSize: {
+    width: 'all',
+    height: 'all'
+  },
+  defaultWhiteBackground: true,
+  shotUrl: ''
 }
 
 
 function logStart() {
-    timeStart = Date.now();
+  timeStart = Date.now();
 };
 
 function logEnd() {
-    timeStop = Date.now();
-    var differenceMs = timeStop - timeStart;
+  timeStop = Date.now();
+  var differenceMs = timeStop - timeStart;
 
-    console.log('Req:', timeStart, 'execution time [ms]:', differenceMs);
+  console.log('Req:', timeStart, 'execution time [ms]:', differenceMs);
 };
 
 function mergeOptions(userOptions) {
 
-    // reset default
-    internalOptions.screenSize.width = 'all';
-    internalOptions.screenSize.height = 'all';
-    internalOptions.shotSize.width = 'all';
-    internalOptions.shotSize.height = 'all';
-    defaultWhiteBackground = true;
-    internalOptions.shotUrl = '//' + ServHost + ':' + ServPort;
+  // reset default
+  internalOptions.screenSize.width = 'all';
+  internalOptions.screenSize.height = 'all';
+  internalOptions.shotSize.width = 'all';
+  internalOptions.shotSize.height = 'all';
+  defaultWhiteBackground = true;
+  internalOptions.shotUrl = '//' + ServHost + ':' + ServPort;
 
-    if (!userOptions) {
-        return internalOptions;
-    }
-    for (var optionKey in internalOptions) {
-        if (optionKey in userOptions) {
-            switch (typeof internalOptions[optionKey]) {
-            case 'boolean':
-                internalOptions[optionKey] = !!userOptions[optionKey];
-                break;
-            case 'number':
-                internalOptions[optionKey] = Math.abs(userOptions[optionKey]);
-                break;
-            case 'string':
-                internalOptions[optionKey] = '' + userOptions[optionKey];
-                break;
-            default:
-                internalOptions[optionKey] = userOptions[optionKey];
-            }
-        }
-    }
+  if (!userOptions) {
     return internalOptions;
+  }
+  for (var optionKey in internalOptions) {
+    if (optionKey in userOptions) {
+      switch (typeof internalOptions[optionKey]) {
+        case 'boolean':
+          internalOptions[optionKey] = !!userOptions[optionKey];
+          break;
+        case 'number':
+          internalOptions[optionKey] = Math.abs(userOptions[optionKey]);
+          break;
+        case 'string':
+          internalOptions[optionKey] = '' + userOptions[optionKey];
+          break;
+        default:
+          internalOptions[optionKey] = userOptions[optionKey];
+      }
+    }
+  }
+  return internalOptions;
 };
 
 
-app.get('/shot', function (req, res) {
-    logStart();
+app.get('/shot', function(req, res) {
+  logStart();
 
-    var params, internalParams, externalParams;
-    try {
-        params = JSON.parse(req.query.params);
-        if (typeof (params.external) !== 'undefined') {
-            externalParams = JSON.stringify(params.external);
-        }
+  var params, internalParams, externalParams;
+  try {
+    params = JSON.parse(req.query.params);
 
-        if (typeof (params.internal) !== 'undefined') {
-            internalParams = params.internal;
-        };
 
-    } catch (err) {
-        console.log('NO JSON in params! ' + err.message);
-
+    if (typeof(params.external) !== 'undefined') {
+      externalParams = JSON.stringify(params.external);
     }
 
-    mergeOptions(internalParams);
-    internalOptions.shotUrl = internalOptions.shotUrl + '/?params=' + externalParams;
+    if (typeof(params.internal) !== 'undefined') {
+      internalParams = params.internal;
+    };
 
-    webshot(internalOptions.shotUrl, internalOptions, function (err, renderStream) {
-        renderStream.pipe(res);
+  } catch (err) {
+    console.log('NO JSON in params! ' + err.message);
 
-        renderStream.on('end', function () {
-            logEnd();
-        });
+  }
 
+  mergeOptions(internalParams);
+  internalOptions.shotUrl = internalOptions.shotUrl + '/?params=' + externalParams;
+
+  webshot(internalOptions.shotUrl, internalOptions, function(err, renderStream) {
+    renderStream.pipe(res);
+
+    renderStream.on('end', function() {
+      logEnd();
     });
+
+  });
+});
+
+app.get('/test', function(req, res) {
+  logStart();
+  webshot('<html><body><h1>Hello World</h1></body></html>', {
+    siteType: 'html'
+  }, function(err, renderStream) {
+    renderStream.pipe(res);
+    renderStream.on('end', function() {
+      logEnd();
+    });
+  });
 });
 
 app.use(express.static(__dirname + '/app'));
 
-var server = app.listen(servPort, servHost, function () {
+var server = app.listen(servPort, servHost, function() {
 
-    ServHost = server.address().address;
-    ServPort = server.address().port;
+  ServHost = server.address().address;
+  ServPort = server.address().port;
 
-    console.log('node-express-webshot-app listening at http://%s:%s', ServHost, ServPort);
+  console.log('node-express-webshot-app listening at http://%s:%s', ServHost, ServPort);
 
 })
